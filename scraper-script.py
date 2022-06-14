d7cd18216606a3a1f433742eb0af3a78bcee66ee
ericyaang <eric_yang@hotmail.com>
2022-06-14 12:10:37 -0300

    main

--- scraper-script.py
+++ scraper-script.py
@@ -1,27 +1,17 @@
-from bs4 import BeautifulSoup
-import requests
+from scraper.scraper_olx import get_infos, get_urls_df
 import pandas as pd
 from datetime import datetime as dt
-from IPython.display import clear_output
-from scraper import get_urls, get_infos, get_urls_df
 
-DATA_PATH = './data'
+DATA_PATH = '/data/olx_'
+PAGES =  1
 
-##
-# Part 1
-##
-centro = get_urls_df('centro')
-continente = get_urls_df('continente')
-leste = get_urls_df('leste')
-norte = get_urls_df('norte')
-sul = get_urls_df('sul')
+centro = get_urls_df('centro', PAGES)
+continente = get_urls_df('continente', PAGES)
+leste = get_urls_df('leste', PAGES)
+norte = get_urls_df('norte', PAGES)
+sul = get_urls_df('sul', PAGES)
 
 df_all = pd.concat([centro, continente, leste, norte, sul], ignore_index=True)
-
-##
-# Part 2
-##
-start_time = dt.now()
 
 infos = []
 for index, row in df_all.iterrows():
@@ -32,11 +22,7 @@ for index, row in df_all.iterrows():
         None
     print(f'Páginas registradas: {index}', end='\r')
 
-time_elapsed = (dt.now() - start_time)
-print(f'Duração: {time_elapsed}')
 
-
-# create df and save
 df = pd.DataFrame(infos)
 now_ = dt.now().strftime('%d%m%y-%H%M%S')
 df.to_csv(DATA_PATH +
